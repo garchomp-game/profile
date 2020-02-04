6bff32498cd77cf46150739d24587d557f9f9e1d
garchomp-game <otoka.japanesedrum@gmail.com>
2020-02-05 01:13:02 +0900

    作業準備整えるまで（pug sass導入w)

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+v-layout(column='', justify-center='', align-center='')
+  v-flex(xs12='', sm8='', md6='')
+    .text-center
+    v-card.top-profile-card
+      v-img.dragonne-center(:src='dragonneImg' aspect-ratio='2' width='250')
+      v-card-title.headline ようこそ！たくの自己紹介ページへ！
+      v-card-text ここにいろいろ書いていくよ！
+      v-card-actions
+        v-spacer
+        v-btn(color='primary', nuxt='', to='/inspire') 自己紹介詳細ページへ
+</template>
+
+<script>
+import Dragonne from '~/assets/images/dragonne.jpg'
+
+export default {
+  data () {
+    return {
+      dragonneImg: Dragonne
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .top-profile-card
+    padding-top: 30px
+  .dragonne-center
+    margin: 0 auto
+</style>
---
+++ pages/inspire.vue
@@ -0,0 +1,7 @@
+<template lang='pug'>
+v-layout
+  v-flex.text-center
+    v-card
+      v-card-title(primary-title='') 詳細ページ
+      v-card-text ここにいろいろ詳細書いてくよ
+</template>
