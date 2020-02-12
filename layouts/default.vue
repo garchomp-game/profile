897ad5a101e4632dfb2384b67a25956323f54803
garchomp-game <otoka.japanesedrum@gmail.com>
2020-02-12 22:44:33 +0900

    簡単なプロフィール文追加するところまで

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,91 +1,28 @@
-<template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+<template lang='pug'>
+v-app(dark='')
+  v-navigation-drawer(
+    v-model='drawer', 
+    :mini-variant='miniVariant', 
+    :clipped='clipped', 
+    fixed='', 
+    app=''
+  )
+    v-list
+      v-list-item(v-for='(item, i) in items', :key='i', :to='item.to', router='', exact='')
+        v-list-item-action
+          v-icon {{ item.icon }}
+        v-list-item-content
+          v-list-item-title(v-text='item.title')
+  v-app-bar(:clipped-left='clipped', fixed='', app='')
+    v-app-bar-nav-icon(@click.stop='drawer = !drawer')
+    v-toolbar-title.toolbar-title-hover(v-text='title' @click='jumpIndex')
+    v-spacer
+  v-content
+    v-container
+      nuxt
+  v-footer(:fixed='fixed', app='')
+    span © 2020
+
 </template>
 
 <script>
@@ -97,21 +34,31 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home',
+          title: 'ホーム',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          icon: 'mdi-format-list-bulleted',
+          title: '自己紹介詳細',
           to: '/inspire'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'たくの自己紹介サイト'
     }
-  }
+  },
+  methods: {
+    jumpIndex() {
+      this.$router.push('/')
+    }
+  },
 }
 </script>
+
+<style lang="sass">
+  .toolbar-title-hover:hover
+    cursor: pointer
+</style>
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,10 +1,10 @@
 <template lang='pug'>
 v-layout
   v-flex.text-center
-    v-card.mx-auto(width='80%')
-      v-card-title(primary-title='') 詳細ページ
-      v-card-text
-        p ここでは自己紹介の詳細と、各キャラの紹介をしていきます！
+    .mx-auto(width='80%')
+      .display-2.my-3.font-weight-bold(primary-title='') 詳細ページ
+      div
+        p.font-weight-bold.font-italic ここでは自己紹介の詳細と、各キャラの紹介をしていきます！
     taku-profile.mt-5
     dragonne-profile.mt-5
 
